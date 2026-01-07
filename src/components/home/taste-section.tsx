dacc3aa16a4ee698a93a94807c56f8f6a9d35822
GhosTHaise <ghostrex2@gmail.com>
2026-01-07 22:11:40 +0300

    feat : taste section image

--- src/components/home/taste-section.tsx
+++ src/components/home/taste-section.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Button } from '../ui/button'
 import { ArrowRight } from 'lucide-react'
+import Image from 'next/image'
 
 const TasteSection = () => {
     return (
         <section className='section-wrapper'>
             <div className="section-container w-full">
-                <div className='flex w-full flex-row lg:flex-col justify-between'>
+                <div className='flex w-full flex-col lg:flex-row justify-between items-center gap-6'>
                     <div className='flex flex-col gap-5 w-full text-left max-w-[657px]'>
                         <h1 className='section_h1 leading-[100%]'>
                             Qu’est-ce qu’on mange aujourd’hui ?
@@ -15,7 +16,7 @@ const TasteSection = () => {
                             Une nouvelle façon de commander auprès de vos restaurants préférés.<br />
                             Simple, rapide et pratique pour vos repas du quotidien.
                         </p>
-                        <div className="flex gap-[14px] items-center">
+                        <div className="flex gap-[14px] items-center max-md:flex-col">
                             <Button className='btn-red_primary'>
                                 Commander maintenant
                                 <ArrowRight />
@@ -25,7 +26,14 @@ const TasteSection = () => {
                             </Button>
                         </div>
                     </div>
-                    <div className='flex-1'></div>
+                    <div className='flex-1'>
+                        <Image
+                            src="/dish_1_square.png"
+                            alt="Dish 1"
+                            width={380}
+                            height={388}
+                        />
+                    </div>
                 </div>
             </div>
         </section>
